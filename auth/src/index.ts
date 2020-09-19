518bf2a50bacd71fddef45819915f155ad95319a
Роман Бубнов <yarepka01@gmail.com>
2020-09-19 17:22:40 +0300

    Startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('I am just in love with startup messages');
+  console.log('Startup message');
   // check if environment variable defined
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
